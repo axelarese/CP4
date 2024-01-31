6584830218c9fd2d5141ed60e3d46f82ff5c1306
AXEL LARESE <axelarese@gmail.com>
2024-01-31 11:48:40 +0100

    1er

---
+++ frontend/src/finalPage/home/NewService.jsx
@@ -0,0 +1,15 @@
+import Add from "../../pages/add/Add";
+import Header from "../../pages/header/Header";
+import Navbar from "../../pages/navBar/NavBar";
+
+function NewService() {
+  return (
+    <>
+      <Header />
+      <Add />
+      <Navbar />
+    </>
+  );
+}
+
+export default NewService;
